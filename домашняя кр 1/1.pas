45fd029d7b532843e0cbdab01b822be7152feb0f
Titoy137 <118978184+Titoy137@users.noreply.github.com>
2022-12-08 20:29:23 +0300

    Add files via upload

---
+++ домашняя кр 1/1.pas
@@ -0,0 +1,14 @@
+﻿var
+  x,o,tg:real;
+  begin
+    tg := sin(x)/cos(x);
+    writeln();
+    readln(x);
+    if x<-7 then
+      o:=cos(x)-x**3
+    else o:= tg/x**3;
+    if (x<5) and (x >=0) then
+    o:=ln(x)*sin(x)
+    else o:=log(x)*tg+log(x)/cos(x);
+    writeln(o);
+  end. 
---
+++ домашняя кр 1/2.pas
@@ -0,0 +1,19 @@
+﻿var
+  i,x,c,tg,o:real;
+  begin
+    writeln('промежуток чисел:');
+    readln(x,i);
+    c:=0.1;
+    tg := sin(x)/cos(x);
+    while x<=i do
+      begin
+      if x<-7 then
+      o:=cos(x)-x**3
+    else o:= tg/x**3;
+    if (x<5) and (x >=0) then
+    o:=ln(x)*sin(x)
+    else o:=log(x)*tg+log(x)/cos(x);
+    writeln(o);
+      x:=x+c;
+      end;
+  end.
